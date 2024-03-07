d2a404e7ed71ffa70c6094fcde3a4e9198e0bbab
huuluc10 <lucnguyenhuu91@gmail.com>
2024-03-07 22:09:55 +0700

    feat: add description for api in swagger

--- src/main/java/com/huuluc/englearn/controller/AdminManageUserController.java
+++ src/main/java/com/huuluc/englearn/controller/AdminManageUserController.java
@@ -2,6 +2,8 @@ package com.huuluc.englearn.controller;
 
 import com.huuluc.englearn.model.User;
 import com.huuluc.englearn.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +12,12 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/admin/v1/manage/user")
+@Tag(name = "Admin API Manage User")
 public class AdminManageUserController {
     private final UserService userService;
 
     @GetMapping("/")
+    @Operation(summary = "Get all users")
     public List<User> findAll() {
         return userService.findAll();
     }
--- src/main/java/com/huuluc/englearn/controller/UserController.java
+++ src/main/java/com/huuluc/englearn/controller/UserController.java
@@ -6,36 +6,42 @@ import com.huuluc.englearn.model.request.CreateUserRequest;
 import com.huuluc.englearn.model.request.UpdateInfoRequest;
 import com.huuluc.englearn.model.response.ResponseModel;
 import com.huuluc.englearn.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/user")
+@Tag(name = "User API")
 public class UserController {
     private final UserService userService;
 
     @PostMapping("/")
+    @Operation(summary = "Create a new account")
     public ResponseEntity<ResponseModel> createAccount(@RequestBody CreateUserRequest request) throws UserException {
         return userService.createUser(request);
     }
 
     @PostMapping("/update")
+    @Operation(summary = "Update user information")
     public ResponseEntity<ResponseModel> updateInfo(@RequestBody UpdateInfoRequest request) throws UserException {
         return userService.updateUser(request);
     }
 
 
     @PostMapping("/getByUsername")
+    @Operation(summary = "Get user information by username")
     public ResponseEntity<ResponseModel> getByUsername(@RequestParam String username) throws IOException {
         return userService.getByUsername(username);
     }
 
     @GetMapping("/getByEmail")
+    @Operation(summary = "Get user information by email")
     public User getByEmail(String email) {
         return userService.getByEmail(email);
     }
