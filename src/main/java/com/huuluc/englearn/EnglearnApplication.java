f73eafccbd08a0f3cf1f2b6b67c8abbc2fe6408d
huuluc10 <lucnguyenhuu91@gmail.com>
2024-03-12 15:06:00 +0700

    docs: Add description about project for swagger

--- src/main/java/com/huuluc/englearn/EnglearnApplication.java
+++ src/main/java/com/huuluc/englearn/EnglearnApplication.java
@@ -1,6 +1,9 @@
 package com.huuluc.englearn;
 
 import com.huuluc.englearn.utils.StorageProperties;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,6 +11,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 //@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
+@OpenAPIDefinition(info = @Info(title = "Englearn API", version = "1.0",
+        description = "Documentation Englearn API v1.0", contact = @Contact(name = "Nguyễn Hữu Lực",
+        email = "lucnguyenhuu91@gmail.com")))
 public class EnglearnApplication {
 
     public static void main(String[] args) {
