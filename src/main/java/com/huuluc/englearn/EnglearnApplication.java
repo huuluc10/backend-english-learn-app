0258314436fa6226e4c0adb7fa9680619cfa3e4c
huuluc10 <lucnguyenhuu91@gmail.com>
2024-03-12 14:35:41 +0700

    docs: remove annotation
    Remove @SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) annotation in EngLearnApplication.java

--- src/main/java/com/huuluc/englearn/EnglearnApplication.java
+++ src/main/java/com/huuluc/englearn/EnglearnApplication.java
@@ -3,11 +3,10 @@ package com.huuluc.englearn;
 import com.huuluc.englearn.utils.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-//@SpringBootApplication
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class EnglearnApplication {
 
